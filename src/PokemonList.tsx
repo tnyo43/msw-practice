5bee1da34b8a370888ac03e5d1b45f325153e7b6
tnyo43 <tnyo43@gmail.com>
2022-08-17 00:36:48 +0900

    add name filter

--- src/PokemonList.tsx
+++ src/PokemonList.tsx
@@ -1,32 +1,40 @@
 import axios from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
 import useSWR from 'swr';
 import { Pokemon } from './types/pokemon';
 import './PokemonList.css';
 
 
-const fetchPokemons = (): Promise<Pokemon[]> => {
-  return axios.get<Pokemon[]>('/pokemons').then((resule) => resule.data);
-}
-
 export const PokemonList: React.FC<{}> = () => {
-  const { data } = useSWR('/api/pokemons', fetchPokemons);
+  const [nameSubstring, setNameSubstring] = useState('');
 
-  if (!data) return <p>loading</p>
+  const fetchPokemons = (): Promise<Pokemon[]> => {
+    return axios.get<Pokemon[]>(`/pokemons${nameSubstring !== '' ? `?name=${nameSubstring}` : ''}`).then((resule) => resule.data);
+  }  
+
+  const { data } = useSWR(`/api/pokemons/${nameSubstring}}`, fetchPokemons);
 
   return (
     <div className="PokemonList-container">
-      <div className="PokemonList-list">
-        {data.map((pokemon) => (
-          <div key={pokemon.id} className="PokemonList-item">
-            <div>No: {pokemon.pokedexNo}</div>
-            <div>name: {pokemon.name}</div>
-            <div className="PokemonList-item-image">
-              <img src={pokemon.image} alt={'pokemon'} />
-            </div>
-          </div>
-        ))}
+      <div>
+        <input onChange={(e) => setNameSubstring(e.target.value)} />
       </div>
+      {!data
+        ? <p>loading</p>
+        : (
+          <div className="PokemonList-list">
+            {data.map((pokemon) => (
+              <div key={pokemon.id} className="PokemonList-item">
+                <div>No: {pokemon.pokedexNo}</div>
+                <div>name: {pokemon.name}</div>
+                <div className="PokemonList-item-image">
+                  <img src={pokemon.image} alt={'pokemon'} />
+                </div>
+              </div>
+            ))}
+          </div>
+        )
+      }
     </div>
   );
 }
--- src/mocks/handlers/pokemonHandler.ts
+++ src/mocks/handlers/pokemonHandler.ts
@@ -1,49 +1,51 @@
 import { rest } from "msw"
 import { Pokemon } from "../../types/pokemon";
 
-type FetchPokemonsParams = {};
+const pokemonData: Pokemon[] = [
+  {
+    id: 'Pokeomon:1',
+    pokedexNo: 1,
+    name: 'Bulbasaur',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
+  },
+  {
+    id: 'Pokeomon:2',
+    pokedexNo: 2,
+    name: 'Ivysaur',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
+  },
+  {
+    id: 'Pokeomon:3',
+    pokedexNo: 3,
+    name: 'Venusaur',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png',
+  },
+  {
+    id: 'Pokeomon:4',
+    pokedexNo: 4,
+    name: 'Charmander',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
+  },
+  {
+    id: 'Pokeomon:5',
+    pokedexNo: 5,
+    name: 'Charmeleon',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
+  },
+  {
+    id: 'Pokeomon:6',
+    pokedexNo: 6,
+    name: 'Charizard',
+    image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
+  },
+]
 
 export const pokemonHandlers = [
-  rest.get<{}, FetchPokemonsParams, Pokemon[]>('/pokemons', (_req, res, ctx) => {
+  rest.get<{}, {}, Pokemon[]>('/pokemons', (req, res, ctx) => {
+    const nameSubstring = req.url.searchParams.get('name')?.toLowerCase();
+
     return res(
-      ctx.json([
-        {
-          id: 'Pokeomon:1',
-          pokedexNo: 1,
-          name: 'Bulbasaur',
-          image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png',
-        },
-        {
-          id: 'Pokeomon:2',
-          pokedexNo: 2,
-          name: 'Ivysaur',
-          image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png',
-        },
-        {
-          id: 'Pokeomon:3',
-          pokedexNo: 3,
-          name: 'Venusaur',
-          image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png',
-        },
-        {
-          id: 'Pokeomon:4',
-          pokedexNo: 4,
-          name: 'Charmander',
-          image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/4.png',
-        },
-        {
-          id: 'Pokeomon:5',
-          pokedexNo: 5,
-          name: 'Charmeleon',
-          image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/5.png',
-        },
-        {
-          id: 'Pokeomon:6',
-          pokedexNo: 6,
-          name: 'Charizard',
-          image: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png',
-        },
-      ])
+      ctx.json(pokemonData.filter((pokemon) => nameSubstring === undefined || pokemon.name.toLowerCase().includes(nameSubstring)))
     )
   })
 ]
